1d40326c9bcf4bb53c389326fa5245f252b9a819
ark948 <armankarimi948@gmail.com>
2024-05-15 21:13:41 +0330

    Added tests

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from todos.models import Todo
 
-# Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "body",
+    )
+
+
+admin.site.register(Todo, TodoAdmin)
