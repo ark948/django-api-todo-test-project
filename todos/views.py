4b088c8f239735b6badaa80739a4f717ed09fabd
ark948 <armankarimi948@gmail.com>
2024-05-15 23:08:29 +0330

    Added API

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from todos.models import Todo
+from todos.serializers import TodoSerializer
 
-# Create your views here.
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
